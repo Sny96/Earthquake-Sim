571c1e3adc515609240de425371a83d8b01e2af4
Sny96 <nat15sn2@student.lu.se>
2019-09-24 17:09:02 +0200

    nya grejjer

--- Project.py
+++ Project.py
@@ -2,27 +2,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-k=9
-mu0=5 #rest friction
-muv=2 #moving frictionduring 20 min
+k=10
+mu0=100 #rest friction
+muv=1 #moving friction
 g=1 #gravity
 m=1 #mass of boxes
-deltat=0.001 #timestep
+deltat=0.01 #timestep
+epsilon=1e-15
+time=150
+n=int(time/deltat)
 def mu(x,v): #returns list
    delx1=x[0]-x[1]
    delx2=x[1]-x[2]
    mu_net=np.array([0.0,0.0,0.0])
    
-   if abs((delx1)*k/(m*g))<=mu0 and v[0]==0:
+   if abs((delx1)*k/(m*g))<=mu0 and v[0]<=epsilon:
       mu_net[0]=((delx1)*k/(m*g))
    else:
-      mu_net[0]=np.sign(v[0])*muv
+      mu_net[0]=-np.sign(v[0])*muv
    
-   if abs((delx2-delx1)*k/(m*g))<=mu0 and v[1]==0:
-      print()
+   if abs((delx2-delx1)*k/(m*g))<=mu0 and v[1]<=epsilon:
       mu_net[1]=(delx2-delx1)*k/(m*g)
    else:
-      mu_net[1]=np.sign(v[1])*muv
+      mu_net[1]=-np.sign(v[1])*muv
    return mu_net
 
 def acc(x,v): #returns 2 accelerations - one for each box (in a list). x, v vectors
@@ -31,38 +33,67 @@ def acc(x,v): #returns 2 accelerations - one for each box (in a list). x, v vect
    delx2=x[1]-x[2]
   
    a=np.array([0.0,0.0,0.0])
-   a[0]=round(-k*(delx1)/m+mu_n[0]*g,10)
-   a[1]=round(-k*(delx2-delx1)/m+mu_n[1]*g,10)
+   a[0]=-k*(delx1)/m+mu_n[0]*g
+   if abs(a[0])<=epsilon:
+      a[0]=0
+   a[1]=-k*(delx2-delx1)/m+mu_n[1]*g
+   if abs(a[1])<=epsilon:
+      a[1]=0
    a[2]=0
+  # print(-k*(delx2-delx1)/m,mu_n[1]*g)
+   
    return a
 
-def eulerx(x,v,h=deltat): #Runga kutta?
-   x=x+v*h
-   return x
+def f(x): #x is a vector of x,v
+   return np.array([x[1], acc(x[0],x[1])])
+   
 
-def eulerv(v,a,h=deltat): #Runga kutta?
-   v=v+a*h
-   return v
+def rk4(x0, h, n):
+    x = np.zeros((n+1,2,3))
 
-x=np.array([0,0,0]) #initial positions
-v=np.array([0,0,1]) #initial velocity
-time=10
-xlist=[]
-x0list=[]
-vlist=[]
-for i in range(int(time/deltat)):
+    x[0] = x0 #set initial conditions
 
-   xlist.append(x[1])
-   x0list.append(x[2])
-   a=acc(x,v)
-   x=eulerx(x,v)
-   v=eulerv(v,a)
+    t=np.linspace(0,h*n,n+1)
+    for i in range(1, n + 1):
+       
+       xi=x[i-1]
+       print(f(xi))
+       k1 = h * f(xi)
+       k2 = h * f(xi + 0.5 * k1)
+       k3 = h * f(xi + 0.5 * k2)
+       k4 = h * f(xi + k3)
+       x[i] = xi + (k1 + 2*k2+ 2*k3 + k4) / 6
+    return x,t
+   
+x_ini=np.array([[0,0,0.1],[0,0,1]]) #initial positions,velocity
+all_data=rk4(x_ini,deltat,n)
+#all_data_list=all_data.tolist()
+timelist=all_data[1]
+zero = np.array([[float(0)]*(n+1)]*2)
+box_1_rel = np.copy(zero) 
+box_2_rel = np.copy(zero) 
 
+box_1 = np.copy(zero) 
+box_2 = np.copy(zero) 
+box_3 = np.copy(zero) 
+print(np.shape(box_1))
+for i,data in enumerate(all_data[0]):
+   box_1[0][i] = data[0][0]
+   box_1[1][i] = data[1][0]
+   box_2[0][i] = data[0][1]
+   box_2[1][i] = data[1][1]
+   box_3[0][i] = data[0][2]
+   box_3[1][i] = data[1][2]
+   box_1_rel[0][i] = data[0][0]-box_3[0][i]
+   box_1_rel[1][i] = data[1][0]-box_3[1][i]
+   box_2_rel[0][i] = data[0][1]-box_3[0][i]
+   box_2_rel[1][i] = data[1][1]-box_3[1][i]
    
    
-tlist=np.linspace(0, time, int(time/deltat))
-plt.plot(tlist,xlist, "b-")
-plt.plot(tlist,x0list, "r-")
+
+plt.plot(timelist, box_2[0])
+plt.plot(timelist, box_1[0])
+#plt.plot(timelist, -box_3[0])
 
    
    
---
+++ Project2.py
@@ -0,0 +1,75 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+k=10
+mu0=100 #rest friction
+muv=1 #moving friction
+g=1 #gravity
+m=1 #mass of boxes
+deltat=0.01 #timestep
+epsilon=1e-15
+time=150
+n=int(time/deltat)
+
+x0=1
+v0=1
+state0=1
+
+eps=0.1
+xi=1
+gamma=1
+
+def f(x): #vector in x = ((state,delta,v))
+   state = x[0]
+   delta = x[1]
+   v = x[2]
+   
+   stateDOT = -v*(state+(1+eps)*np.log(v))
+   deltaDOT = v-1
+   vDOT = -gamma**2*(delta+(1/xi)*(state+np.log(v)))
+
+   return np.array([stateDOT, deltaDOT, vDOT])
+   
+
+def rk4(x0, h, n):
+    x = np.zeros((n+1,3)) #creating np.array where we save our data
+
+    x[0] = x0 #set initial conditions
+
+    t=np.linspace(0,h*n,n+1)
+    
+    for i in range(1, n + 1):
+       
+       xi=x[i-1]
+       k1 = h * f(xi)
+       k2 = h * f(xi + 0.5 * k1)
+       k3 = h * f(xi + 0.5 * k2)
+       k4 = h * f(xi + k3)
+       x[i] = xi + (k1 + 2*k2+ 2*k3 + k4) / 6
+    return x,t
+   
+x_ini=np.array([1,1,1]) #initial positions,velocity
+all_data=rk4(x_ini,deltat,n)
+#all_data_list=all_data.tolist()
+timelist=all_data[1]
+zero = np.array([[float(0)]*(n+1)]*2)
+
+stateEVO = [x[0] for x in all_data[0]]
+deltaEVO = [x[1] for x in all_data[0]]
+vEVO = [x[2] for x in all_data[0]]
+   
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+   
+ax.plot( xs=stateEVO, ys=deltaEVO, zs=vEVO)
+ax.set_xlabel('state')
+ax.set_ylabel('delta')
+ax.set_zlabel('velocity')
+
+fig2 = plt.figure()
+plt.plot(timelist, stateEVO, color="green", label="state")
+plt.plot(timelist, deltaEVO, color="blue", label="delta")
+plt.plot(timelist, vEVO, color="red", label="velocity")
+plt.legend()
+
